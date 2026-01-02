3af406a487da33b97a3d4ffe2273406d0b1e7680
Seval Karaosmanoğlu <sevallacin@gmail.com>
2026-01-02 09:19:13 +0000

    orders_margin ara model

---
+++ models/intermediate/int_orders_margin.sql
@@ -0,0 +1,10 @@
+SELECT
+orders_id,
+MAX(date_date) as date_date,
+ROUND(sum(revenue),2) as revenue,
+ROUND(sum(quantity),2) as quantity,
+ROUND(sum(purchase_cost),2) as purchase_cost,
+ROUND(sum(margin),2) as margin
+from {{ ref("int_sales_margin") }}
+GROUP BY orders_id, date_date
+ORDER BY orders_id DESC
